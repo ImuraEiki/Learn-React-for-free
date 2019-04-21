a10cdbcf886cf966ce862979f858c681428681e9
ImuraEiki <iimura1560222@gmail.com>
2019-04-21 21:58:50 +0900

    ほんとにちょっとずつ進めていくんだな

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react"
+import React,{Component} from "react"
 
 // https://scrimba.com/p/p4Mrt9/cQnMDHD
 
-class App extends React.Component {
-  // initialize some value
-    constructor() {
-        super()
-        this.state = {
-          answer: "Yes!"
-        }
+class App extends Component {
+    constructor(){
+      super()
+      this.state = {
+        name: "Sally",
+        age: 13
+      }
     }
-    
+
     render() {
         return (
             <div>
-                <h1>Is state important to know? {this.state.answer}</h1>
-                <ChildComponent answer={this.state.answer} />
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} yaers old</h3>
             </div>
         )
     }
