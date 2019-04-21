872a9361833a637da3bb34a899a936c793b4d065
ImuraEiki <iimura1560222@gmail.com>
2019-04-21 20:17:18 +0900

    constがあるコンポーネントはReact.Componentとしないらしいし、importの書き方も変わる

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React from 'react'
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
+/*
 
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
+
+*/
+
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+
+// #1
 class App extends React.Component {
-  yourMethodHere(){
-    
-  }
-
   render(){
-    return(
-      <div>
-        <h1>{this.props.whatever}</h1>
-      </div>
+    return (
+        <div>
+            <Header username="vschool"/>
+            <Greeting />
+        </div>
     )
   }
 }
 
+// #2
+class Header extends React.Component {
+  render(props){
+    return (
+        <header>
+            <p>Welcome, {this.props.username}!</p>
+        </header>
+    )
+  }
+}
 
-export default App;
+// #3
+class Greeting extends Component{
+  render(){
+
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+
+
+      return (
+          <h1>Good {timeOfDay} to you, sir or madam!</h1>
+      )
+    
+  }
+}
+
+ReactDOM.render(
+    <App />,
+     document.getElementById("root"))
+
+
+export default App
