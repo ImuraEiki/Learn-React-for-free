08152a887aa2110e56a28a6b413fa12541f44524
ImuraEiki <iimura1560222@gmail.com>
2019-04-22 22:09:20 +0900

    onDragEnter

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function handleClick() {
     console.log("I was clicked")
 }
 
+// https://reactjs.org/docs/events.html#supported-events
+
 function App() {
     return (
         <div>
-            <img src="https://www.fillmurray.com/200/100"/>
+            <img onDragEnter={() => console.log("Hovered!")} src="https://www.fillmurray.com/200/100"/>
             <br />
             <br />
             <button onClick={handleClick}>Click me</button>
