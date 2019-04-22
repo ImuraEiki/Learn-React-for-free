01842d310f8ec8bef16c7b33c489323f701122bb
ImuraEiki <iimura1560222@gmail.com>
2019-04-22 21:55:15 +0900

    functionからclassへ

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import './style.css';
 import TodoItem from './TodoItem'
 import todosData from './todosData'
 
-function App(){
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />
-  )
-  
-  return(
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  )
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />
+    )
+    
+    return(
+      <div className="todo-list">
+        {todoItems}
+      </div>
+    )
+  }
 }
 
 export default App;
